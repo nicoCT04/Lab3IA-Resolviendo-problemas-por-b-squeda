add13c2865a2c3eef6fbb709111a94afdbae777a
nicoCT04 <nicoconcua@gmail.com>
2026-03-04 09:49:30 -0600

    inciso a y b hechos

--- main.py
+++
@@ -1,9 +0,0 @@
-from treeNode import TreeNode
-from fifo import FIFOQueue
-from lifo import LIFOQueue
-from priorityQueue import PriorityQueue
-from datos import cargar_datos
-
-# Ahora ya puedes inicializar todo
-grafo, heuristica = cargar_datos()
-frontera = PriorityQueue(use_heuristic=True) # Para A*
